3611d1ab50dd6f1e80103b984dc9a75fd0a9aca9
wangcf <18388627773@163.com>
2018-11-23 19:10:30 +0800

    1.2

--- monitor/urls.py
+++ monitor/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from mail import views as mali_vi
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^template/', mali_vi.template),
 ]
