25a037699a282e7b891ac0360ace6527649f5dd3
40829 <408297372@qq.com>
2018-11-24 13:49:02 +0800

    test

--- monitor/settings.py
+++ monitor/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'xadmin.apps.XadminConfig',
     'user_management',
+    'testApp.apps.TestappConfig',
 ]
 
 MIDDLEWARE = [
@@ -72,15 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'monitor.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 数据库的类型
