083dc7d3adf819b129003786dd2900181209c74a
40829 <408297372@qq.com>
2018-11-24 15:32:26 +0800

    排除了一个bug

--- middlewares/xadminMiddleware.py
+++ middlewares/xadminMiddleware.py
@@ -18,7 +18,7 @@ class CheckXadminLogin(MiddlewareMixin):
     @staticmethod
     def process_request(request):
         split_urls = request.path_info.split("/")
-        if split_urls[1]:
+        if split_urls[1] == "xadmin":
             if request.user.is_anonymous:
                 error_msg = "请您先登陆！"
                 return render(request, "xadmin/xadmin_login.html", {"error_msg": error_msg})
