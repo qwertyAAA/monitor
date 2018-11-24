dbd43e612ccdb66731ded84d27b55cd995da9098
czw <czw@hxak.com>
2018-11-24 12:51:47 +0800

    uaer数据库

--- user_management/models.py
+++ user_management/models.py
@@ -1,26 +1,21 @@
-
 from django.db import models
 from django.contrib.auth.models import User
-
-# 用户信息表
+# 用户模型.
+# 第一种：采用的继承方式扩展用户信息（本系统采用）
+# 扩展：关联的方式去扩展用户信息
 class UserInfo(models.Model):
-    # user_id = models.AutoField(primary_key=True)
-    user = models.OneToOneField(to=User, to_field="id", verbose_name="用户")
     user_address = models.CharField(max_length=32, null=True, verbose_name="地址")
-    user_grade=models.CharField(max_length=32, null=True, verbose_name="等级")
-    user_phone = models.CharField(max_length=16, null=True, verbose_name="电话")
-    Due_time=models.CharField(max_length=16, null=True, verbose_name="到期时间")
-    user_stay_years=models.CharField(max_length=16, null=True, verbose_name="年限")
+    user_phone = models.CharField(max_length=16, unique=True, verbose_name="电话")
+    user_stay_years = models.IntegerField(null=True, verbose_name="年限")
+    user_status = models.BooleanField(default=True, verbose_name="状态")
     user_gender = models.CharField(max_length=4, verbose_name="性别")
-    user_age = models.IntegerField(null=True, verbose_name="编号")
-    user_number = models.CharField(max_length=16, null=True, verbose_name="电话")
-    user_remarks=models.CharField(max_length=32, null=True, verbose_name="备注")
-    user_recentip=models.CharField(max_length=16, null=True, verbose_name="最近的ip")
-    def __str__(self):
-        return self.user.username
-
-
-
+    user_age = models.IntegerField(null=True, verbose_name="年龄")
+    user_number = models.CharField(max_length=20, unique=True, verbose_name="编号")
+    user_remarks = models.CharField(max_length=255, null=True, verbose_name="备注")
+    user_recent_ip = models.CharField(max_length=16, verbose_name="最近的ip")
+    user_id_card = models.CharField(max_length=20, unique=True, verbose_name="身份证号")
+    user_image = models.ImageField(upload_to="image", default="image/default.jpg", verbose_name="头像")
+    user = models.OneToOneField(to=User)
 
 
 
