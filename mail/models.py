0edaff4cc90e9195e7acc00e82dc6f412ca745b4
czw <czw@hxak.com>
2018-11-27 16:08:16 +0800

    增删改查

--- mail/models.py
+++ mail/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from user_management.models import UserInfo
 
 # Create your models here.
 class Pictures(models.Model):
@@ -8,13 +8,13 @@ class Pictures(models.Model):
     path = models.FileField(upload_to="avatars/", default="avatars/default.jpg", verbose_name="图片地址")
     BZ = models.CharField(max_length=50, verbose_name="备注")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
-    master_id = models.ForeignKey(to=User)
+    master_id = models.ForeignKey(to=UserInfo)
 
 
 class Fhsms(models.Model):
     content = models.CharField(max_length=9999, verbose_name="邮件内容")
-    to_user = models.ForeignKey(to=UserInfo, verbose_name="发送人")
-    from_user = models.ForeignKey(to=User, verbose_name="接收人")
+    to_user = models.CharField(max_length=32, verbose_name="发送人")
+    from_user = models.ForeignKey(to=UserInfo, verbose_name="接收人")
     create_time = models.DateTimeField(auto_now_add=True, verbose_name="发送时间")
     status = models.CharField(max_length=32, verbose_name="邮件状态", choices=((0, '未读'),(1, '已读')))
 
