1e7746b94f3b4328d4ab1e1894b8347861412117
AllenLeepy <1424749544@qq.com>
2018-11-30 14:38:00 +0800

    修复功能

--- monitor/settings.py
+++ monitor/settings.py
@@ -52,8 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'middlewares.xadminMiddleware.CheckXadminPermission',
-    # 'permission.service.rbac.ValidPermission',
+    'middlewares.xadminMiddleware.CheckXadminPermission',
+    'permission.service.rbac.ValidPermission',
 ]
 
 ROOT_URLCONF = 'monitor.urls'
