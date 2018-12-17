70d3134fd2e637f2d79464729ceb08aa09246aea
Lovecraft <judesming@gmail.com>
2018-12-17 15:08:36 +0800

    简报1.5

--- report/models.py
+++ report/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Mould(models.Model):
+    name = models.CharField(max_length=20)
+    url = models.TextField(max_length=100)
+    key = models.CharField(max_length=100)
+    status = models.BooleanField(default=False)
+
+
+class Report(models.Model):
+    name = models.CharField(max_length=30, default='舆情简报')
+    create_time = models.DateField(auto_now_add=True)
+    tieba_num = models.IntegerField()
+    blog_num = models.IntegerField()
+    sensitive = models.IntegerField()
+    no_sensitive = models.IntegerField()
+    mould = models.ForeignKey(to=Mould)
--- report/urls.py
+++ report/urls.py
@@ -12,5 +12,11 @@ urlpatterns = [
     url(r'^delete/', views.delete),
     url(r'^lot_delete/', views.lot_delete),
     url(r'^del_all/', views.del_all),
-
+    url(r'^append/', views.append),
+    url(r'^collection_delete/', views.collection_delete),
+    url(r'^lot_append/', views.lot_append),
+    url(r'^cmould/', views.cmould),
+    url(r'^create_report/', views.create_report),
+    url(r'^rep_detail/', views.rep_detail),
+    url(r'^del_rep/', views.del_rep),
 ]
